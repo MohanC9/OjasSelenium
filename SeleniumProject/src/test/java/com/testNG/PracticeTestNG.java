e9c07dc52a518ce4ec5e0285f8fe993553d113a2
= <=>
2021-07-31 10:27:53 +0530

    hiiii

---
+++ SeleniumProject/src/test/java/com/testNG/PracticeTestNG.java
@@ -0,0 +1,29 @@
+package com.testNG;
+
+import org.testng.annotations.Test;
+
+public class PracticeTestNG {
+	
+	
+	@Test
+	public void First() {
+
+		System.out.println("First Method Passed");
+	}
+	
+	
+	@Test
+	public void Second() {
+		System.out.println("Second Method Passed");
+
+	}
+	
+	@Test
+	public void Third() {
+		System.out.println("Third Method Passed");
+
+	}
+	
+	
+
+}
